046f4fb9a59be4dc45c0c20132fa99aa55bfb543
Robert (laptop) <robert.abela.15@um.edu.mt>
2019-09-17 23:00:21 +0200

    update constants

--- src/main/java/com/robertabela/rss/Constants.java
+++ src/main/java/com/robertabela/rss/Constants.java
@@ -1,4 +1,4 @@
-package com.robertabela.rss.lidl;
+package com.robertabela.rss;
 
 import java.text.SimpleDateFormat;
 
@@ -6,10 +6,12 @@ public class Constants {
 
 	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE, dd/MM/yyyy");
 	
-	public static final String BASE_URL = "https://www.lidl.com.mt";
-	public static final String START_URL = BASE_URL+"/en/non-food.htm";
-
 	public static final String AUTHOR = "missing-rss-feeds";
 
+	public static final String LIDL_BASE_URL = "https://www.lidl.com.mt";
+	public static final String LIDL_START_URL = LIDL_BASE_URL+"/en/non-food.htm";
 	public static final String LIDL_LOGO = "https://www.lidl.com.mt/statics/lidl-offering-mt/ds_img/layout/brand__default.svg";
+	
+	public static final String TOM_BASE_URL = "https://twitter.com/TheTimesofMalta";
+	public static final String TOM_TWITTER_HANDLE = "TheTimesofMalta";
 }
