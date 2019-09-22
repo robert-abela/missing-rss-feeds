fba73e7340bfb63cd253f243a240c87e515f2274
Robert (laptop) <robert.abela.15@um.edu.mt>
2019-09-22 07:29:37 +0200

    Implemented API key using environment variable "APIKEY" and "key=XXX" HTTP GET parameter

--- src/main/java/com/robertabela/rss/RssFeedContoller.java
+++ src/main/java/com/robertabela/rss/RssFeedContoller.java
@@ -1,7 +1,18 @@
 package com.robertabela.rss;
 
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.View;
 
@@ -10,20 +21,45 @@ import com.robertabela.rss.tom.TomRssFeedView;
 
 @RestController
 public class RssFeedContoller {
- 
-    @Autowired
-    private LidlRssFeedView lidlView;
-     
-    @Autowired
-    private TomRssFeedView tomView;
 
-    @GetMapping("/lidl")
-    public View getLidlFeed() {
-        return lidlView;
-    }
+	@Autowired
+	private LidlRssFeedView lidlView;
 
-    @GetMapping("/tom")
-    public View getTomFeed() {
-        return tomView;
-    }
+	@Autowired
+	private TomRssFeedView tomView;
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private String feedApiKey;
+
+	@GetMapping("/lidl")
+	public View getLidlFeed(@RequestParam("key") String key, HttpServletResponse resp) throws IOException {
+		return checkKey(lidlView, key, resp);
+	}
+
+	@GetMapping("/tom")
+	public View getTomFeed(@RequestParam("key") String key, HttpServletResponse resp) throws IOException {
+		return checkKey(tomView, key, resp);
+	}
+
+	private View checkKey(View view, String key, HttpServletResponse resp) throws IOException {
+		if (feedApiKey == null) {
+	        resp.sendError(SC_INTERNAL_SERVER_ERROR, "Failed to load environment variable: APIKEY");
+	        return null;
+		}
+		else if (!feedApiKey.equals(key)) {
+	        resp.sendError(SC_UNAUTHORIZED, "Key sent in request not accepted");
+	        return null;
+		}
+		else {
+			return view;
+		}
+	}
+
+	@PostConstruct
+	private void firstRun() {
+		feedApiKey = System.getenv().get("APIKEY");
+		if (feedApiKey == null)
+			logger.error("Failed to load environment variable: APIKEY");
+	}
 }
